bff1f84c5e19ba7a614beffef3e9f57da832092a
Nicolas Orchow <nico@bitex.la>
2018-12-11 14:58:47 -0300

    Remove deleted file from import

--- test/mock-servers/index.js
+++ test/mock-servers/index.js
@@ -1,5 +1,4 @@
 import mockFundingServer from './mock-funding-server'
-import mockBotsServer from './mock-bots-server'
 import mockMerchantServer from './mock-merchant-server'
 
-export { mockFundingServer, mockBotsServer, mockMerchantServer }
+export { mockFundingServer, mockMerchantServer }
